49814eab09cff25fe98c25440132ac877771e4d2
topiaspe <topiaspe@metropolia.fi>
2022-05-27 14:30:07 +0300

    exercise 11.9 second test added

--- cypress/integration/Pokedex.js
+++ cypress/integration/Pokedex.js
@@ -4,4 +4,14 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('individual pokemon (ivysaur) can be opened', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+
+    cy.get('a[href="/pokemon/ivysaur"]').click()
+    cy.contains('chlorophyll')
+    cy.contains('ivysaur')
+  })
 })
