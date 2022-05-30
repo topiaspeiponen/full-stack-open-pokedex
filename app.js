a36c7cf7fae51308f48f0d8d587b25a36d26776a
topiaspe <topiaspe@metropolia.fi>
2022-05-30 11:26:00 +0300

    exercise 11.16 testing again #skip

--- app.js
+++ app.js
@@ -16,5 +16,5 @@ app.get('/version', (req, res) => {
 
 app.listen(PORT, () => {
   /* eslint-disable no-console */
-  console.log('server indeeddyylyyafaaa stasdftarated on portda 5000')
+  console.log('server indeeddyylyyafaaaa stasdftarated on portda 5000')
 })
